5709115516b80ecd26a836da4547de3d63fd1fe8
Egemen Erin <pixelsushidev@hotmail.com>
2024-10-02 16:35:58 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int aa=5;
+        int bb=6;
+        int cc=5;
+        if ((aa == bb && aa != cc) || (aa == cc && aa != bb) || (bb == cc && bb != aa) || (aa == bb && aa == cc)) {
+            System.out.println("Error");
+        }
+    }
+}
